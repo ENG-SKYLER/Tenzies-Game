41d23935437497193e7ca73ae54b28d6a67778a0
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-10-23 14:28:08 +0200

    init game

---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+import './index.css'
+import { layer } from "@fortawesome/fontawesome-svg-core"
+
+export default function App() {
+    const [count , setCount] =  React.useState(0)
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    const [isActive, setIsActive] = React.useState(true);
+    const [seconds, setSeconds] = React.useState(0);
+    const [minutes , setMinutes] = React.useState(0)
+    // React.useEffect(() => {
+    //   let timer = null;
+    //   if(isActive){
+    //     timer = setInterval(() => {
+    //       setSeconds((seconds) => seconds + 1);
+    //     }, 1000);
+    //   }
+    let timer
+    React.useEffect(()=>{
+        if(isActive){
+           timer = setInterval(()=>{
+                setSeconds(seconds + 1);
+            }, 1000)
+            if(seconds === 59){
+                setMinutes(minutes + 1)
+                setSeconds(0)
+            }
+        }
+        return ()=>{
+            clearInterval(timer)
+        }
+    })
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+        }
+    }, [isActive])
+
+
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie()
+            }))
+            setCount(count + 1)
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+            setCount(0)
+            setSeconds(0)
+            setMinutes(0)
+        }
+        
+    }
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+   
+    
+    return (
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button 
+                className="roll-dice" 
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </button>
+            <div className="roll-tracker">
+                <h3> No Of Rolls : {count} </h3>
+                <h3> Time :  {minutes < 10 ? "0" + minutes : minutes}:{seconds < 10 ? "0" + seconds : seconds}</h3>
+            </div>
+        </main>
+    )
+}
+
+
+
---
+++ src/Die.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiceOne, faDiceTwo, faDiceThree , faDiceFour , faDiceFive , faDiceSix} from '@fortawesome/free-solid-svg-icons'
+import { counter } from "@fortawesome/fontawesome-svg-core"
+
+
+export default function Die(props) {
+function diceRendering(){
+
+    if(props.value === 1){
+        return faDiceOne
+    }else if(props.value === 2){
+        return faDiceTwo
+    }else if(props.value === 3){
+        return faDiceThree
+    }else if(props.value === 4){
+        return faDiceFour
+    }else if(props.value === 5){
+        return faDiceFive
+    }else {
+        return faDiceSix
+    }
+}
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    return (
+        <div 
+        className="die-face" 
+        style={styles}
+        onClick={props.holdDice}
+        >  
+            <FontAwesomeIcon className="dice-icon" icon={diceRendering()}  />
+        </div>
+    )
+}
